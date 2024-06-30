3517042f14ae78d7af9001f81e1a72d64b0ee1ce
Nicolas Chomyn <chomynnicolas2@gmail.com>
2024-06-30 11:16:40 -0400

    configurando el envio del formulario del front

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,5 +1,10 @@
+const multer = require('multer');
 const EventControllers = require('../controllers/event.controller');
+
+const upload = multer({ storage: multer.memoryStorage() });
+
 
 module.exports = (app) => {
     app.get('/api', EventControllers.index);
+    app.post('/upload', upload.single('imagen'), EventControllers.subiendoEventos)
 }
